362dc3a512c06128589d083f6ca7af6a74727fb4
Jefferson Alquinga <jefferson.alquinga1998@gmail.com>
2024-04-15 19:24:41 -0500

    Final de sql fundamentos

--- introduccion.sql
+++ introduccion.sql
@@ -57,4 +57,62 @@ SELECT*FROM cliente WHERE nombre = "Jefferson Alquinga";
 
 UPDATE clientes SET nombre ="Jefferson Chuquimarca" , ci = 143453462345, WHERE id = 2;
 
---
+--ELIMINAR DATOS DE TABLA COMMENT elimianra registros de una tabla 
+
+DELETE FROM servicios WHERE id = 1;
+
+--PARA ELIMINAR UNA COLUMNA;
+ALTER TABLE servicios DROP COLUMN descripcion;
+
+
+--ELIMINAR TABLA COMMENT
+DROP TABLE servicios;
+
+--PARA REALIZAR CONSULTA UTILIZAMOS COMMENT
+
+SELECT*FROM servicios WHERE precio BETWEEN 299 AND 499;
+
+
+--FUNCIONES AGREGADORAS
+
+SELECT COUNT(id), fecha FROM reservaciones GROUP BY fecha ORDER BY COUNT(id)DESC;
+
+--FUNCIONES PARA CREAR UN OPERARCION Y UNA TABLA VIRTULA CON AS
+
+SELECT SUM(precio) AS precioTotla FROM servicios;
+SELECT MAX(precio)AS maxprecio FROM servicios;
+SELECT MIN(precio) AS minprecio FROM servicios;
+
+
+--PARA PODER BUSCAR UTILIZAMOS LIKE PARA ALGO ESPECIFICAO
+
+SELECT *FROM reservaciones WHERE nombre LIKE 'cabello';
+
+
+
+--COMANDO PARA UNIR DOS COLUMNA COMMENT
+
+SELECT CONCAT(nombre,' ', apellido) AS NombresCompletos FROM servicios;
+
+
+--COMANDO PARA UNIR DOS COLUMAS Y REALIZAR UN BUSQUEDA CON LIKE 
+
+SELECT hora,fecha, CONCAT(nombre,' 'apellido)AS NombreCompletos FROM reservaciones WHERE CONCAT(nombre,' ',apellido) LIKE '%Ana Preciado';
+
+
+--CREAR RELACIONES CON TABLAS EXISTEN 3 NORMALIZACIONES QUE DEBEMOS SEGUIE LA 1 NO EXISTE DATOS DUPLICADOS
+--EN UNA MISMA COLUMAN, 2 NO SE DEBE DUPLICAR DATOS EN UNA MISMA TABLA, 3 NO SE PUEDE REALIZAR CALCULO DE PAGOS.COMMENT
+
+-- CREAMOS CONDICIONES PARA TABLA DE CITAS COMMENT
+
+CREATE TABLE citas (
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    fecha DATE NOT NULL,
+    hora  TIME NOT NULL,
+    clienteId INT(11) NOT NULL,
+    PRIMARY KEY(id),
+    KEY clienteId(clienteId),
+    CONSTRAINT cliente_FK 
+    FOREIGN KEY(clienteId)
+    REFERENCES clientes(id)
+);
